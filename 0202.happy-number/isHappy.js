4bc87436005c2411ee09ee300a79c5aa3c6c2d4e
Maosen Chen <maosen1993@gmail.com>
2019-11-11 14:16:00 -0500

    problem 202 (#13)

---
+++ 0202.happy-number/isHappy.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    let visited = {}
+    while (n !== 1) {
+        if (visited[n]) {
+            return false
+        }
+        visited[n] = true
+        n = sumOfSquares(n)
+    }
+    return true
+}
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumOfSquares = function(n) {
+    let sum = 0
+    let currNum = n
+    while (currNum > 0) {
+        sum += Math.pow(currNum % 10, 2)
+        currNum = Math.floor(currNum / 10)
+    }
+    return sum
+}
+
+module.exports = isHappy
+// runtime: 68 ms 45%
+// memory: 35 MB 465
