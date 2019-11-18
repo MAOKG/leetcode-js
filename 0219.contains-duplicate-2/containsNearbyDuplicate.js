8ae0072984df2df1bab831a142255fe795233841
Maosen Chen <maosen1993@gmail.com>
2019-11-17 21:04:41 -0500

    problem 219 (#25)

---
+++ 0219.contains-duplicate-2/containsNearbyDuplicate.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const set = new Set()
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return true
+        }
+        set.add(nums[i])
+        if (i >= k) {
+            set.delete(nums[i - k])
+        }
+    }
+    return false
+}
+
+module.exports = containsNearbyDuplicate
+// runtime 83%
+// memory 84%
