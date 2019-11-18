6b1289a3703e001924db2536d6d8932f8604a148
Maosen Chen <maosen1993@gmail.com>
2019-11-17 23:32:02 -0500

    problem 206 (#27)

---
+++ 0206.reverse-linkedlist/reverseList.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let curr = head
+    let prev = null
+    while (curr) {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    return prev
+}
+
+// runtime 15%
+// memory 74%
