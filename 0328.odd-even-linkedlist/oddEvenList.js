d23bec1d0ab594f140e9aad29df1db1fbe258fb5
Maosen Chen <maosen1993@gmail.com>
2019-11-21 23:27:16 -0500

    problem 328 (#32)

---
+++ 0328.odd-even-linkedlist/oddEvenList.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if (!head) {
+        return null
+    }
+    let oddTail = head
+    let evenHead = null
+    let evenTail = null
+    let i = 2
+    let curr = head.next
+
+    while (curr) {
+        if (i % 2 === 0) {
+            if (evenTail) {
+                evenTail.next = curr
+                evenTail = curr
+            } else {
+                evenTail = curr
+                evenHead = curr
+            }
+        } else {
+            oddTail.next = curr
+            oddTail = curr
+        }
+        i++
+        curr = curr.next
+    }
+    if (evenTail) {
+        evenTail.next = null
+    }
+    oddTail.next = evenHead
+    return head
+}
+module.exports = oddEvenList
+// runtime 10%
+// memory 100%
