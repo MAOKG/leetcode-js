8bcd4f49aade86d3f5b98a8d7fde9ca59fe47714
maosenchen6ix <mchen@solution-tek.com>
2019-11-23 22:53:39 -0500

    add test case

--- 0025.reverse-nodes-in-kgroup/reverseKGroup.test.js
+++ 0025.reverse-nodes-in-kgroup/reverseKGroup.test.js
@@ -10,10 +10,10 @@ test('test case 1', () => {
     expect(linkedListToArray(result)).toEqual([2, 1, 4, 3, 5])
 })
 
-// test('test case 1', () => {
-//     var l = arrayToLinkedList([1, 2, 3, 4, 5])
-//     var k = 3
-//     var result = reverseKGroup(l, k)
+test('test case 1', () => {
+    var l = arrayToLinkedList([1, 2, 3, 4, 5])
+    var k = 3
+    var result = reverseKGroup(l, k)
 
-//     expect(linkedListToArray(result)).toEqual([3, 2, 1, 4, 5])
-// })
+    expect(linkedListToArray(result)).toEqual([3, 2, 1, 4, 5])
+})
