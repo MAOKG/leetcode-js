37cf039309eb2fb753045098f337fe736a30d317
Maosen Chen <maosen1993@gmail.com>
2021-08-06 20:38:00 -0400

    Problem 300 (#130)
    * DP solution
    
    * minor update

---
+++ 0300.longest-increasing-subsequence/lengthOfLIS.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const n = nums.length
+    if (n === 0) {
+        return 0
+    }
+    const memo = new Array(n).fill(1)
+    let res = 1
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j] && memo[i] < 1 + memo[j]) {
+                memo[i] = 1 + memo[j]
+            }
+        }
+        if (memo[i] > res) {
+            res = memo[i]
+        }
+    }
+    return res
+}
+
+module.exports = lengthOfLIS
+// runtime 76%
+// memory 44%
