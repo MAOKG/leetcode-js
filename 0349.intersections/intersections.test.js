7953900289b4ae294e1902e540c3267fc2aa84f9
Maosen <maosen1993@gmail.com>
2023-01-29 01:22:09 +0800

    fix test

--- 0349.intersections/intersections.test.js
+++ 0349.intersections/intersections.test.js
@@ -4,12 +4,15 @@ test('test case 1', () => {
     var nums1 = [1, 2, 2, 1]
     var nums2 = [2, 2]
 
-    expect(intersection(nums1, nums2)).toEqual([2])
+    const result = intersection(nums1, nums2)
+    result.sort((a, b) => a - b)
+    expect(result).toEqual([2])
 })
 
 test('test case 2', () => {
     var nums1 = [4, 9, 5]
     var nums2 = [9, 4, 9, 8, 4]
-
-    expect(intersection(nums1, nums2)).toEqual([9, 4])
+    const result = intersection(nums1, nums2)
+    result.sort((a, b) => a - b)
+    expect(result).toEqual([4, 9])
 })
